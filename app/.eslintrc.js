9aa520c686c50e378a0fd85c43b50f2ef9d5b14b
Aryella Lacerda <aryella.lacerda@gmail.com>
2020-10-27 14:39:49 -0300

    chore: configure eslint to work with prettier

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,9 +1,16 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
   settings: {
     react: {
       version: 'detect'
     }
+  },
+  extends: [
+    '@react-native-community',
+    "plugin:prettier/recommended"
+  ],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error"
   }
 }
